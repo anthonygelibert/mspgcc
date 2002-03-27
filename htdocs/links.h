5c2a3ec452cc66f22d2f33f04924e2dcce77ae1d
cliechti <cliechti>
2002-03-27 01:47:41 +0000

    Initial revision

---
+++ htdocs/links.h
@@ -0,0 +1,17 @@
+<!-- -*- html -*- -->
+<h3>Subsections</h3>
+<li><a href="index.html">Introduction</a>
+<li><a href="build.html">Build</a>
+<li><a href="tools.html">Tools</a>
+<li><a href="ref.html">References</a>
+
+<h3>Useful</h3>
+<LI><A href="baudrate.html">BaudrateCalc</A>
+
+<h3>Exits</h3>
+<LI><A href="http://www.ti.com/sc/msp430">MSP430@ti.com</A>
+<LI><A href="http://gcc.gnu.org/">GCC&nbsp;home</A>
+<LI><A href="http://sources.redhat.com/binutils/">Binutils&nbsp;home</A>
+<LI><A href="http://python.org/">Python.org</A>
+<LI><A href="http://cygwin.com/">Cygwin</A>
+

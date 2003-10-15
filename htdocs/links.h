bee6b81f1b2a294cc2a2f776d6f3d41ec2892929
cliechti <cliechti>
2003-10-15 22:36:31 +0000

    https -> http link to idex.html in manual/faq  dirs

--- htdocs/links.h
+++ htdocs/links.h
@@ -2,9 +2,9 @@
 <h3>Subsections</h3>
 <li><a href="index.html">Introduction</a>
 <li><a href="news.html">News</a>
-<li><a href="manual/book1.html">Documentation</a>
+<li><a href="manual/">Documentation</a>
 <li><a href="tools.html">Tools</a>
-<li><a href="faq/t2.html">FAQ</a>
+<li><a href="faq/">FAQ</a>
 <li><a href="contribute.html">Contribute</a>
 <li><a href="bugreports.html">Reporting bugs</a>
 <li><a href="ref.html">Links</a>
